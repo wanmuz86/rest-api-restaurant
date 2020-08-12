b7b52273d52910098c050e4b31a3737d3eff9b19
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-08-12 21:32:41 +0800

    add item

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Restaurant = require('./restaurant')
+const User = require('./user')
 mongoose.connect('mongodb+srv://apiuser:abcd1234@cluster0.agh0w.mongodb.net/rest-api?retryWrites=true&w=majority')
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -35,7 +36,7 @@ router.post('/restaurants',(req,res)=>{
 	}).catch(err=>{
 		res.err({message:'Something is wrong'})
 	})
-	
+
 })
 router.get('/restaurants',(req,res)=>{
 	Restaurant.find()
@@ -44,7 +45,7 @@ router.get('/restaurants',(req,res)=>{
 	}).catch(err=>{
 		res.err({message:'Something is wrong'})
 	})
-	
+
 })
 router.get('/restaurants/:id',(req,res)=>{
 	Restaurant.findById(req.params.id)
@@ -53,7 +54,7 @@ router.get('/restaurants/:id',(req,res)=>{
 	}).catch(err=>{
 		res.err({message:'Something is wrong'})
 	})
-	
+
 })
 
 router.put('/restaurants/:id',(req,res)=>{
@@ -63,7 +64,7 @@ router.put('/restaurants/:id',(req,res)=>{
 	}).catch(err=>{
 		res.err({message:'Something is wrong'})
 	})
-	
+
 })
 
 router.delete('/restaurants/:id', (req,res)=>{
@@ -73,8 +74,56 @@ router.delete('/restaurants/:id', (req,res)=>{
 	}).catch(err=>{
 		res.err({messsage:'Something is wrong'})
 	})
-	
+
 })
+
+// This is Create
+router.post('/register', (req,res)=>{
+// Create
+var newUser = new User({
+	username:req.body.username,
+	password:req.body.password
+})
+newUser.save().then(doc=>{
+
+	res.json({message:'User succesfully registered!'})
+}).catch(err=>{
+	res.json({message:'An error occured '+err})
+})
+});
+
+//This is Read- for login, we need to use POST because it is more secure
+router.post('/login', (req,res)=>{
+
+// Readd
+User.findOne({username:req.body.username}).then(user=>{
+	console.log(user)
+	if (user){
+	// if (req.body.password == user.password){
+	// 	res.json({message:'Authenticated'});
+	// }
+	// else {
+	// 	res.json({message:'Wrong password'})
+	// }
+	user.verifyPassword(req.body.password,function(err,isMatch){
+		if (err) res.json({message:'Something is wrong'})
+			if (!isMatch){
+				res.json({message:'Wrong password'})
+			}
+			else {
+				res.json({message:'Authenticated'})
+			}
+	})
+}
+else {
+	res.json({message:'User not found!'})
+}
+}).catch(err=>{
+	res.json({message:'An error occured '+err});
+})
+})
+
+
 
 app.use('/api',router);
 
---
+++ user.js
@@ -0,0 +1,34 @@
+let mongoose = require('mongoose');
+let bcrypt = require('bcrypt-node');
+let userSchema = new mongoose.Schema({
+	username:{type:String, unique:true, required:true},
+	password:{type:String, required:true}
+})
+
+userSchema.pre('save', function(callback){
+	var user = this;
+
+	if (!user.isModified('password')) return callback();
+
+	bcrypt.genSalt(5, (err,salt)=>{
+		if (err) return callback(err);
+
+		bcrypt.hash(user.password, salt, null, (err,hash)=>{
+			if (err) return callback(err);
+			user.password = hash;
+			callback();
+		})
+	})
+})
+// It will check if the encrypted password inside out date 
+// is an encryption of password sent
+
+//It will not decrypt - > Normally a password that is hashed + salt will take years to be decrypted
+userSchema.methods.verifyPassword = function(password, callback){
+	bcrypt.compare(password,this.password, function(err,isMatch){
+		if(err) return callback(err);
+		callback(null, isMatch);
+	})
+}
+
+module.exports = mongoose.model('User',userSchema);
